ebad5b315971ab4c6267f8a60441b29375086b66
DikiyHimik <sanyamakarov1997@gmail.com>
2020-12-15 17:36:48 +0400

    Create Game

---
+++ Assets/Scripts/GameScene/GameItem/GameItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameItem : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(-_speed * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<ScoreZone>(out ScoreZone scoreZone))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
